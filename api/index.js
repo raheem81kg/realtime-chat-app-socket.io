d84c91e9be8509ebeedd23420dcd74eb5f6ea4d7
raheem81kg <104041730+raheem81kg@users.noreply.github.com>
2023-08-07 14:15:01 -0500

    hosting bug fixes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const colors = require("colors");
-const cors = require("cors");
 require("dotenv").config();
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDb = require("./config/db");
