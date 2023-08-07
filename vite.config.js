23ea31d51ad124f23ee0f44fadfc82486a93bd79
raheem81kg <104041730+raheem81kg@users.noreply.github.com>
2023-08-07 11:05:28 -0500

    hosting bug fixes

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,19 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+        proxy: {
+            "/graphql": {
+                target: "https://realtime-chat-app-raheem81kg.vercel.app:5000/graphql",
+                changeOrigin: true,
+                secure: true,
+                ws: false,
+            },
+            "/socket.io": {
+                target: "https://realtime-chat-app-gxyp.onrender.com",
+                changeOrigin: true,
+                secure: true,
+                ws: true,
+            },
+        },
     },
 });
