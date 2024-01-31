d963f6c088986ccc81e08ce2e3284da6dd621dc4
Raheem Gordon <raheemgor81@gmail.com>
2024-01-31 02:11:57 -0500

    shortening load times for users

--- socket/index.js
+++ socket/index.js
@@ -8,86 +8,91 @@ require("dotenv").config();
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+   res.send("Hello, World!");
+});
+
+// jump start socket server early from client-side
+app.get("/ping", (req, res) => {
+   res.send("Socket server is running!");
 });
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: process.env.REACT_APP_URL,
-        methods: ["GET", "POST"],
-    },
+   cors: {
+      origin: process.env.REACT_APP_URL,
+      methods: ["GET", "POST"],
+   },
 });
 
 let users = [];
 
 const addUser = (user, socketId) => {
-    const { usersName, randomImage } = user;
-    const existingUser = users.find((user) => user.usersName === usersName);
-    if (existingUser) {
-        existingUser.socketId = socketId;
-    } else {
-        users.push({ usersName, randomImage, socketId });
-    }
+   const { usersName, randomImage } = user;
+   const existingUser = users.find((user) => user.usersName === usersName);
+   if (existingUser) {
+      existingUser.socketId = socketId;
+   } else {
+      users.push({ usersName, randomImage, socketId });
+   }
 };
 
 const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
+   users = users.filter((user) => user.socketId !== socketId);
 };
 
 io.on("connection", (socket) => {
-    console.log(`${socket.id} has connected`);
+   console.log(`${socket.id} has connected`);
 
-    socket.on("addUser", (user) => {
-        addUser(user, socket.id);
+   socket.on("addUser", (user) => {
+      addUser(user, socket.id);
 
-        io.emit("getUsers", users);
-    });
+      io.emit("getUsers", users);
+   });
 
-    socket.on("disconnect", () => {
-        console.log(`${socket.id} has disconnected`);
-        removeUser(socket.id);
-        io.emit("getUsers", users);
-    });
+   socket.on("disconnect", () => {
+      console.log(`${socket.id} has disconnected`);
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+   });
 
-    socket.on("joinRoom", (chatId) => {
-        socket.join(chatId);
-    });
+   socket.on("joinRoom", (chatId) => {
+      socket.join(chatId);
+   });
 
-    socket.on("sendMessageToRoom", (messageData) => {
-        // Broadcast the message to the room based on the chatId
-        socket.to(messageData.chatId).emit("receiveMessage", messageData);
-    });
+   socket.on("sendMessageToRoom", (messageData) => {
+      // Broadcast the message to the room based on the chatId
+      socket.to(messageData.chatId).emit("receiveMessage", messageData);
+   });
 
-    socket.on("sendMessage", (messageData) => {
-        const { id, chatId, sender, message, createdAt, receiver, randomImage } = messageData;
+   socket.on("sendMessage", (messageData) => {
+      const { id, chatId, sender, message, createdAt, receiver, randomImage } = messageData;
 
-        // Perform basic validations on the required fields
-        if (!id || !chatId || !sender || !message || !createdAt) {
-            // Handle invalid message data (e.g., log an error, send an error response)
-            console.error("Invalid message data:", messageData);
-            return;
-        }
+      // Perform basic validations on the required fields
+      if (!id || !chatId || !sender || !message || !createdAt) {
+         // Handle invalid message data (e.g., log an error, send an error response)
+         console.error("Invalid message data:", messageData);
+         return;
+      }
 
-        // Check if the sender is online (present in the users array)
-        const recipient = users.find((user) => user.usersName === receiver);
-        if (recipient) {
-            // User is online, emit the message to their socket
-            io.to(recipient.socketId).emit("receiveMessage", {
-                id,
-                chatId,
-                sender,
-                message,
-                createdAt,
-                randomImage,
-            });
-        } else {
-            // User is offline, handle the message accordingly (e.g., store in the database)
-            // ...
-        }
-    });
+      // Check if the sender is online (present in the users array)
+      const recipient = users.find((user) => user.usersName === receiver);
+      if (recipient) {
+         // User is online, emit the message to their socket
+         io.to(recipient.socketId).emit("receiveMessage", {
+            id,
+            chatId,
+            sender,
+            message,
+            createdAt,
+            randomImage,
+         });
+      } else {
+         // User is offline, handle the message accordingly (e.g., store in the database)
+         // ...
+      }
+   });
 });
 
 const PORT = process.env.SOCKET_PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+   console.log(`Server running on port ${PORT}`);
 });
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,69 +2,77 @@ import React, { useState, useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import PuffLoader from "react-spinners/PuffLoader";
-
+import axios from "axios";
 import { UserContext } from "../App";
 import { ADD_MEMBER_TO_CHAT } from "../mutation/chatMutations";
 import LoginForm from "../components/LoginForm";
 import images from "../data/images";
 
 const Login = () => {
-    const { user, setUser } = useContext(UserContext);
-    const [loginName, setLoginName] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [addMemberToChat] = useMutation(ADD_MEMBER_TO_CHAT);
+   const { user, setUser } = useContext(UserContext);
+   const [loginName, setLoginName] = useState("");
+   const [loading, setLoading] = useState(false);
+   const [addMemberToChat] = useMutation(ADD_MEMBER_TO_CHAT);
 
-    const getRandomImage = () => {
-        const randomIndex = Math.floor(Math.random() * images.length);
-        return images[randomIndex].imageUrl;
-    };
+   const getRandomImage = () => {
+      const randomIndex = Math.floor(Math.random() * images.length);
+      return images[randomIndex].imageUrl;
+   };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+   const handleSubmit = async (e) => {
+      e.preventDefault();
 
-        if (loginName.trim() !== "") {
-            setLoading(true);
-            try {
-                await addMemberToChat({
-                    variables: { userName: loginName.trim(), chatId: "64a9972a6861cd1a093f3c15" },
-                });
-            } catch (error) {
-                // Handle error if the mutation fails
-                // console.error("Mutation error:", error);
-            }
+      if (loginName.trim() !== "") {
+         setLoading(true);
+         try {
+            await addMemberToChat({
+               variables: { userName: loginName.trim(), chatId: "64a9972a6861cd1a093f3c15" },
+            });
+         } catch (error) {
+            // Handle error if the mutation fails
+            // console.error("Mutation error:", error);
+         }
 
-            setTimeout(() => {
-                setLoading(false);
-                const randomImage = getRandomImage();
-                setUser({ name: loginName.trim(), randomImage: randomImage });
-            }, 200);
-        }
-    };
+         setTimeout(() => {
+            setLoading(false);
+            const randomImage = getRandomImage();
+            setUser({ name: loginName.trim(), randomImage: randomImage });
+         }, 200);
+      }
+   };
 
-    const handleNameChange = (e) => {
-        setLoginName(e.target.value);
-    };
+   useEffect(() => {
+      pingServer();
+      // Change the URL to the root path when the component mounts
+      // this is to prevent servers giving error when you reload on /login
+      window.history.pushState({}, "", "/");
+   }, []);
 
-    useEffect(() => {
-        // Change the URL to the root path when the component mounts
-        // this is to prevent servers giving error when you reload on /login
-        window.history.pushState({}, "", "/");
-    }, []);
+   const pingServer = async () => {
+      try {
+         console.log("first");
+         const response = await axios.get(`${import.meta.env.VITE_SOCKET_URI}/ping`);
+         console.log(response.data); // Should log "Server is running!"
+      } catch (error) {
+         console.error("Error pinging the server:", error);
+      }
+   };
 
-    return !user ? (
-        <div>
-            {!loading ? (
-                <LoginForm loginName={loginName} setLoginName={setLoginName} handleSubmit={handleSubmit} />
-            ) : (
-                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-                    <PuffLoader color="black" size={80} />
-                </div>
-            )}
-        </div>
-    ) : (
-        // If user is logged in, redirect to the Home page
-        <Navigate to="/" />
-    );
+   return !user ? (
+      <div>
+         {!loading ? (
+            <LoginForm loginName={loginName} setLoginName={setLoginName} handleSubmit={handleSubmit} />
+         ) : (
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+               <PuffLoader color="black" size={80} />
+               <p style={{ marginTop: "10px", fontSize: "14px", color: "gray" }}>May take a while to load the first time...</p>
+            </div>
+         )}
+      </div>
+   ) : (
+      // If user is logged in, redirect to the Home page
+      <Navigate to="/" />
+   );
 };
 
 export default Login;
